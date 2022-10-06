2e02c1ae99359b69b64a4cf325b428421e959f32
Rakugen <simonlee0306@gmail.com>
2022-10-06 07:31:21 -0400

    main.py: fleshed out home and get_blog routes post.py: 1st attempt to create dynamic post based on input num index.html: 1st attempt to create working links to individual posts

--- post.py
+++ post.py
@@ -1,8 +1,8 @@
 class Post:
-    def __init__(self):
-
-
-        pass
-
+    def __init__(self, post_id, title, subtitle, body):
+        self.post_id = post_id
+        self.title = title
+        self.subtitle = subtitle
+        self.body = body
 
     pass
