0d5b5a90953e52af44409f05d4954e0502105217
Rakugen <simonlee0306@gmail.com>
2022-10-06 07:31:21 -0400

    main.py: fleshed out home and get_blog routes post.py: 1st attempt to create dynamic post based on input num index.html: 1st attempt to create working links to individual posts

--- main.py
+++ main.py
@@ -2,24 +2,33 @@ from flask import Flask, render_template
 from post import Post
 import requests
 
-
 app = Flask(__name__)
 
+# Creating list of all posts to be used in home() and blog() functions
+post_list = []
+response = requests.get(url="https://api.npoint.io/c790b4d5cab58020d391")
+
+for post in response.json():
+    new_post = Post(post["id"], post["title"], post["subtitle"], post["body"])
+    post_list.append(new_post)
+
+# TODO: modify home page to show blog names and links
+#   then have the links redirect to its individual blog page
 @app.route('/')
 def home():
-    return render_template("index.html")
+
+    return render_template("index.html", posts=post_list)
 
 # TODO: create get_blog route and render all blog posts from npoint https://api.npoint.io/c790b4d5cab58020d391
-@app.route('/blog')
-def get_blog():
-    post_list = []
-    response = requests.get(url="https://api.npoint.io/c790b4d5cab58020d391")
-    posts = response.json()
-    for post in posts:
-        new_post = Post()
-        post_list.append(new_post)
+@app.route('/blog/<num>')
+def get_blog(num):
+    # Search post_list for a post with id of num
+    found_post = 0
+    for current_post in post_list:
+        if current_post.post_id == num:
+            found_post = current_post
 
-    pass
+    return render_template("post.html", post=found_post)
 
 
 if __name__ == "__main__":
